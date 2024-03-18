c88e32a5bdd0719bdcebdd6240f1efb3d7452d52
DanielHemmis <daniel.hemmis@proton.me>
2024-03-19 00:44:56 +0100

    Changed aiming to mouse instead of automatic

--- 2d-project-start/gun.gd
+++ 2d-project-start/gun.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
-func _physics_process(_delta):
-	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range.front()
-		look_at(target_enemy.global_position)
-		
+func _process(_delta):
+	look_at(get_global_mouse_position())
+
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
